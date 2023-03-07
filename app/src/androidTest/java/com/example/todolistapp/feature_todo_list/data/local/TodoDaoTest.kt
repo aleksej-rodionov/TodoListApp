3bbb240a1bbfb654dc63cd633cd055aa330bc3c0
Aleksej <67003544+aleksej-rodionov@users.noreply.github.com>
2023-03-07 13:22:47 +0600

    added small dao test

---
+++ app/src/androidTest/java/com/example/todolistapp/feature_todo_list/data/local/TodoDaoTest.kt
@@ -0,0 +1,65 @@
+package com.example.todolistapp.feature_todo_list.data.local
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.todolistapp.TodoListApp
+import com.example.todolistapp.feature_todo_list.data.local.mapper.toTodoEntity
+import com.example.todolistapp.feature_todo_list.domain.model.Todo
+import com.google.common.truth.Truth.assertThat
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class TodoDaoTest {
+
+    var compDisp: CompositeDisposable? = null
+
+    private lateinit var database: TodoDatabase
+    private lateinit var dao: TodoDao
+
+    @Before
+    fun setUp() {
+        compDisp = CompositeDisposable()
+
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            TodoDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        dao = database.dao()
+    }
+
+    @After
+    fun tearDown() {
+        compDisp?.clear()
+
+        database.close()
+    }
+
+    @Test
+    fun insertTodoWorksProperly() {
+        val todo = Todo(text = "testTodo")
+        dao.insertTodo(todo.toTodoEntity()).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                dao.getTodoById(it.toInt()).subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        assertThat(it.text).isEqualTo(todo.text)
+                    }, {
+                        it.printStackTrace()
+                    }).apply { compDisp?.add(this) }
+            }, {
+                it.printStackTrace()
+            }).apply { compDisp?.add(this) }
+    }
+}
